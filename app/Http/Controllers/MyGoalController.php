b955c89d11c2d42eb22325ac593d9f3f2211e7b9
rc1021 <rc.701021@gmail.com>
2018-12-31 07:28:26 +0800

    goal modify

--- app/Http/Controllers/MyGoalController.php
+++ app/Http/Controllers/MyGoalController.php
@@ -43,6 +43,7 @@ class MyGoalController extends Controller
     public function store(Request $request)
     {
         $this->rep($request);
+        return response('Hello World', 200)->header('Content-Type', 'text/plain');
     }
 
     /**
