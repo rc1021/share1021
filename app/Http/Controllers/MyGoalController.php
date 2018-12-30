cd74aaf2ff1ffbbef61069a1f66aaf56ae69dc2c
rc1021 <rc.701021@gmail.com>
2018-12-31 07:24:10 +0800

    goal modify

---
+++ app/Http/Controllers/MyGoalController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Goal;
+use App\Repositories\MyGoalRepository;
+use Illuminate\Http\Request;
+
+class MyGoalController extends Controller
+{
+    private $rep;
+
+    function __construct(MyGoalRepository $rep) {
+        $this->rep = $rep;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->rep($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Goal $goal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Goal $goal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Goal $goal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Goal $goal)
+    {
+        //
+    }
+}
