da77f3396b97b4fe792612a7911ac789cdd1259d
rc1021 <rc.701021@gmail.com>
2018-12-31 08:24:16 +0800

    goal modify

--- app/Repositories/MyGoalRepository.php
+++ app/Repositories/MyGoalRepository.php
@@ -18,44 +18,44 @@ class MyGoalRepository
     
     public function store(Request $request)
     {
-        $client = new LINEBotTiny($this->channelAccessToken, $this->channelSecret);
-        foreach ($client->parseEvents() as $event) {
-            switch ($event['type']) {
-                case 'message':
-                    $message = $event['message'];
-                    $source = $event['source'];
-                    switch ($message['type']) {
-                        case 'text':
-                            // 記在資料庫
-                            $new_goal = new Goal;
-                            $new_goal->year = date('Y');
-                            $new_goal->text = $message['text'];
-                            if($source['type'] == "user")
-                                $new_goal->userId = $source['userId'];
+        // $client = new LINEBotTiny($this->channelAccessToken, $this->channelSecret);
+        // foreach ($client->parseEvents() as $event) {
+        //     switch ($event['type']) {
+        //         case 'message':
+        //             $message = $event['message'];
+        //             $source = $event['source'];
+        //             switch ($message['type']) {
+        //                 case 'text':
+        //                     // 記在資料庫
+        //                     $new_goal = new Goal;
+        //                     $new_goal->year = date('Y');
+        //                     $new_goal->text = $message['text'];
+        //                     if($source['type'] == "user")
+        //                         $new_goal->userId = $source['userId'];
 
-                            $new_goal->save();
+        //                     $new_goal->save();
 
-                            // 通知聽友 OK
-                            $client->replyMessage([
-                                'replyToken' => $event['replyToken'],
-                                'messages' => [
-                                    [
-                                        'type' => 'text',
-                                        'text' => "已更新年計劃\n".$message['text']."\n---\n並放入時光彩裡"
-                                    ]
-                                ]
-                            ]);
-                            break;
-                        default:
-                            error_log('Unsupported message type: ' . $message['type']);
-                            break;
-                    }
-                    break;
-                default:
-                    error_log('Unsupported event type: ' . $event['type']);
-                    break;
-            }
-        };
+        //                     // 通知聽友 OK
+        //                     $client->replyMessage([
+        //                         'replyToken' => $event['replyToken'],
+        //                         'messages' => [
+        //                             [
+        //                                 'type' => 'text',
+        //                                 'text' => "已更新年計劃\n".$message['text']."\n---\n並放入時光彩裡"
+        //                             ]
+        //                         ]
+        //                     ]);
+        //                     break;
+        //                 default:
+        //                     error_log('Unsupported message type: ' . $message['type']);
+        //                     break;
+        //             }
+        //             break;
+        //         default:
+        //             error_log('Unsupported event type: ' . $event['type']);
+        //             break;
+        //     }
+        // };
     }
 
     public function show(Goal $goal)
