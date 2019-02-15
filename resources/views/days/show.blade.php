897b06bc5aad69d1a652bdf7d3f72878e2178c1b
yiming.hsu <mufasa.hsu@gmail.com>
2019-02-15 13:46:56 +0800

    加入Adsence

--- resources/views/days/show.blade.php
+++ resources/views/days/show.blade.php
@@ -57,6 +57,14 @@
     gtag('config', 'UA-134198588-1');
     </script>
 
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+              google_ad_client: "ca-pub-9965291835469846",
+              enable_page_level_ads: true
+        });
+    </script>
+
 </head>
 <body>
     
