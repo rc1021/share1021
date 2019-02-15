94a66c52e0d5e3f09396672a0edc32e54874fbe2
yiming.hsu <mufasa.hsu@gmail.com>
2019-02-15 12:38:41 +0800

    加入簡易SEO

--- resources/views/days/show.blade.php
+++ resources/views/days/show.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$row->title}}</title>
+    <meta name="description" content="{{str_limit(strip_tags($row->content), 50)}}" />
+    <meta name="google" content="nositelinkssearchbox" />
+    <meta property="og:url"                content="{{url()->full()}}" />
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content="{{$row->title}}" />
+    <meta property="og:description"        content="{{str_limit(strip_tags($row->content), 50)}}" />
 
     <link href="//stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <style>
