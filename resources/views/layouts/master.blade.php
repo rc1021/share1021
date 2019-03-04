712c88d6aac0295c5914515f5e4341407902906b
yiming.hsu <mufasa.hsu@gmail.com>
2019-03-04 15:34:45 +0800

    修改廣告內容

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -95,6 +95,8 @@
           </div>
         </nav>
 
+    @include("shared._adsense")
+
     @yield('content')
 
     <script src="//code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
