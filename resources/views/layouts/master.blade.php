f55458e86e50a4f1ba1159c26da87dd590b25368
yiming.hsu <mufasa.hsu@gmail.com>
2019-03-05 09:36:30 +0800

    修改網頁結構

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -72,9 +72,9 @@
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
           <div class="container">
-            <a class="navbar-brand" href="/"><i class="fa fa-home" aria-hidden="true"></i></a>
+            <a class="navbar-brand" href="/"><i class="fa fa-home" aria-hidden="true"></i>Day List</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <i class="fa fa-bars"></i>Day List
+              <i class="fa fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
               <ul class="navbar-nav ml-auto">
