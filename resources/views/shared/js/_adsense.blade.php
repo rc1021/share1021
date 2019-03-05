6711c877234ff4450d2c2b70051bfa521bb9b314
rc1021 <rc.701021@gmail.com>
2019-03-05 23:45:23 +0800

    fixup 更新聽說第2季網站

--- resources/views/shared/js/_adsense.blade.php
+++ resources/views/shared/js/_adsense.blade.php
@@ -1,10 +1,2 @@
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-              google_ad_client: "ca-pub-9965291835469846",
-              enable_page_level_ads: true
-        });
-    </script>
-
     <script async custom-element="amp-auto-ads" src="//cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
