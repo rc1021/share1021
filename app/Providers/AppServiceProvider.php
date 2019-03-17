70a64a52dc2cc8f6b89359480010d12a6327e18f
rc1021 <rc.701021@gmail.com>
2019-03-17 12:59:03 +0800

    S3 setting...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \URL::forceScheme('https');
         \Schema::defaultStringLength(191);
     }
 
